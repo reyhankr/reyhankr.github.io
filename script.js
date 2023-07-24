d82b743398808b8c8368aa2ce31c580da0814350
Reyhan Kharisma Ramadhan <rayhan.kharisma@gmail.com>
2023-07-25 02:03:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function initengahan(){
   },}).go();
   }
   function aktipesan4(){
-    wallpaper.style="transform: scale(1.5);";kalimat.innerHTML=pesan4.innerHTML + "Nursyifa" + " 🥳";kalimat.style="transform:scale(1.2)";setTimeout(aktipesan5,1000);
+    wallpaper.style="transform: scale(1.5);";kalimat.innerHTML=pesan4.innerHTML + "Cintaku, Nursyifa Tiara Dwiputri" + " 🥳";kalimat.style="transform:scale(1.2)";setTimeout(aktipesan5,1000);
   }
   vketik5=pesan5.innerHTML;pesan5.innerHTML="";
   function aktipesan5(){
