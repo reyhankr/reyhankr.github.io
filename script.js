b7b83476d04054ae22ce8bdf542006d5f67f81cc
Reyhan Kharisma Ramadhan <rayhan.kharisma@gmail.com>
2023-07-25 02:35:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ function initengahan(){
   document.getElementById("lv4").onclick = function() {lv4.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
   var slov=0;function checkslov() {if(slov==4){kolombaru.style="position:relative;transform:scale(1)";otomatis();setTimeout(aktipesan2,400);}}
   
-  async function pertanyaan(){var { isConfirmed: prtanya } = await swals.fire({title: nama + ' ' + tanya, text: '' + opstanya, imageUrl: '' + fotostiker6.src, showCancelButton: true, confirmButtonText: '' + tompositif, cancelButtonText: '' + tomnegatif,});
+  async function pertanyaan(){var { isConfirmed: prtanya } = await swals.fire({title: "Ayang" + ' ' + tanya, text: '' + opstanya, imageUrl: '' + fotostiker6.src, showCancelButton: true, confirmButtonText: '' + tompositif, cancelButtonText: '' + tomnegatif,});
     if(prtanya){
   await swalst.fire({title: '' + katatambahan.innerHTML, timer: 2000, imageUrl: '' + stikerditolak.src,});
       vketik8=vketik81;aktipesan8();
